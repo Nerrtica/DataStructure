9cf8214e61c9a0f6b54119ce0cc69c5f2e243d2b
jooh95 <jooh95@naver.com>
2015-01-12 15:02:42 +0900

    오준오

---
+++ Queue_01/오준오.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#define SIZE 5
+int front=0;
+int rear=0;
+int queue[SIZE];
+
+int push(int data){
+	if(rear==SIZE-1){
+		rear=0;
+		printf("queue is full\n");
+	}
+	else{
+		queue[rear]=data;
+		rear++;
+	}
+}
+
+int pop(){
+	if(front==SIZE-1){
+		front=0;
+		printf("queue is empty\n");
+	}
+	else{
+		queue[front]=NULL;
+		front++;
+	}
+	return 0;
+}
+
+//int isEmpty(int *queue1){
+//	if(front==rear){
+//		printf("queue is empty\n");
+//		return 1;
+//	}
+//	else{
+//		return 0;
+//	}
+//}
+//
+//int isFull(int *queue1){
+//	if(rear==SIZE){
+//		printf("queue is full\n");
+//		return 1;
+//	}
+//	else{
+//		return 0;
+//	}
+//}
+
+int main(void){
+	int i;
+	int menu;
+	int data;
+	while(1){
+		printf("1.push 2.pop\n");
+		printf("choose menu: ");
+		scanf("%d",&menu);
+		if(menu==1){
+			printf("input a data to push: ");
+			scanf("%d",&data);
+			push(data);
+			for(i=0;i<SIZE;i++){
+				printf("%d",queue[i]);
+			}
+			printf("\n");
+		}
+		else{
+			printf("pop the data\n");
+			pop();
+			for(i=0;i<SIZE;i++){
+				printf("%d",queue[i]);
+			}
+			printf("\n");
+		}
+	}
+	return 0;
+}
