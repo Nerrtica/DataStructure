3eb8b09056adc7252a3c8e781691d40a956529f5
EmiyaMuliZomZzada <gombabo1217@naver.com>
2015-01-23 12:03:24 +0900

    유재범
    드디어 다했다 다익스트라.

--- Graph_03/유재범.c
+++ Graph_03/유재범.c
@@ -4,7 +4,7 @@
 #pragma warning(disable:4996)
 #define SIZE 7
 
-void Dijkstra(int graph[SIZE][SIZE], int cost[SIZE], int parents[SIZE], int end);
+void Dijkstra(int graph[SIZE][SIZE], int end);
 
 int main(void){
 	int end = 0;
@@ -16,19 +16,54 @@ int main(void){
 		{0, 0, 0, 4, 0, 0, 10}, 
 		{0, 0, 0, 0, 0, 0, 1}, 
 		{0, 0, 0, 0, 0, 0, 0}};
-	int cost[SIZE] = {0, };
-	int parents[SIZE] = {0, };
+	
 	printf("어디까지 가시겠습니까, 호갱님? : ");
 	scanf("%d", &end);
-	Dijkstra(graph, cost, parents, end);
+	Dijkstra(graph, end);
 
 	return 0;
 }
 
-void Dijkstra(int graph[SIZE][SIZE], int cost[SIZE], int parents[SIZE],int end){
-	int i = 0, j = 0, save = 0;
+void Dijkstra(int graph[SIZE][SIZE],int end){
+	int i = 0, j = 0, save = 999, point = 1, min = 100;
+	int finalPoint[SIZE];
+	int cost[SIZE];
+	int parents[SIZE];
 	int node[SIZE] = {0, };
-
+	for(i = 0; i < SIZE; i++){
+		cost[i] = 999;
+		parents[i] = 999;
+		finalPoint[i] = 1;
+	}
+	parents[0] = 0;
+	cost[0] = 0;
+	finalPoint[0] = 0;
+	finalPoint[SIZE - 1] = 0;
+	for(i = 0; i < SIZE - 1; i++){
+		min = 100;
+		for(j = 0; j < SIZE; j++){
+			if(graph[point - 1][j] > 0 && cost[j] > cost[point - 1] + graph[point - 1][j]){
+				parents[j] = point;
+				cost[j] = cost[point - 1] + graph[point - 1][j];
+			}
+			if(j != 0 && parents[j] == point && min > cost[j]){
+				min = cost[j];
+				save = j;
+			}
+		}
+		finalPoint[point - 1] = 0;
+		point = save + 1;
+		if(point == end){
+			for(j = 0; j < SIZE; j++){
+				if(finalPoint[j] > 0){
+					point = j + 1;
+					break;
+				}
+			}
+		}
+	}
+	
+	/*
 	for(i = 0; i < SIZE - 1; i++){
 		for(j = 0; j < SIZE; j++){
 			save = graph[i][j] + cost[i];
@@ -42,7 +77,7 @@ void Dijkstra(int graph[SIZE][SIZE], int cost[SIZE], int parents[SIZE],int end){
 			}
 		}
 	}
-
+	*/
 	printf("최소 코스트 : %d\n", cost[end - 1]);
 	node[0] = end;
 	j = end;
